2682f560a23b0dcd042d528ae8ea31ee0212331a
test001 <test001@163.com>
2019-05-16 14:44:42 +0800

    update file

--- Run.py
+++ Run.py
@@ -2,13 +2,13 @@
 
 import unittest
 import time
-from HTMLTestReport import HTMLTestRunner
+from HTMLTestReportCN import HTMLTestRunner
 
 dirpath = './scripts'
 discover = unittest.defaultTestLoader.discover(dirpath, pattern='*_tc.py')
 currenttime = time.strftime('%y%m%d%H%M%S ')
 filedir = './reports/' + 'report' + currenttime + '.html'
-fp = open(filedir, 'wb')
+fp = open(filedir, 'w',encoding='utf8')
 runner = HTMLTestRunner(stream=fp, title=u'计算器自动化测试报告', description=u'计算器自动化测试项目详细描述')
 runner.run(discover)
 fp.close()
